bb373387beed175e8238316b6463f38dba170540
mraksoll <mraksoll@gmail.com>
2020-08-30 15:07:06 +0300

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -191,11 +191,10 @@ public:
             if (halvings >= 64)
             return 0;
             CAmount nSubsidy = 50 * COIN;
-            // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
-            else if (blockHeight >= % consensusParams.superblock == 0)
-                return consensusParams.proposalMaxAmount + 1 * COIN;
-            return 1 * COIN;
 			nSubsidy >>= halvings;
+            // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
+            if (blockHeight >= % consensusParams.superblock == 0)
+                return consensusParams.proposalMaxAmount + 1 * COIN;
             return nSubsidy;
         };
 
